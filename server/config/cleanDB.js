52c34234b141174f9dc4c8db9c39447408cb3d26
Brian Whisler <brian.whisler@gmail.com>
2024-02-18 21:30:31 -0700

    remove log

--- server/config/cleanDB.js
+++ server/config/cleanDB.js
@@ -6,7 +6,6 @@ module.exports = async (modelName, collectionName) => {
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
     }).toArray()
-    console.log(modelExists);
     if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
