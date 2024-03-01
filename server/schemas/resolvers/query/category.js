0dd8f04953bac8d94050de0453801b2bf46a2aff
Brian Whisler <brian.whisler@gmail.com>
2024-02-29 19:00:18 -0700

    finish moving resolvers to folder

---
+++ server/schemas/resolvers/query/category.js
@@ -0,0 +1,23 @@
+const { CategoryName, CategoryType } = require("../../../models");
+const { AuthenticationError } = require("../../../utils/auth");
+
+const categories = async (_, __, context) => {
+  if (context.user) {
+    let userCategories = await CategoryName.find({ user: context.user._id })
+      .populate('categoryType')
+      .sort({ categoryType: 1 })
+      .sort({ categoryName: 1 });
+    return userCategories;
+  }
+  throw new AuthenticationError('You are not authenticated');
+}
+
+const categoryTypes = async (_, __, context) => {
+  if (context.user) {
+    let userCategoryTypes = await CategoryType.find({ user: context.user._id }).sort({ categoryType: 1 });
+    return userCategoryTypes;
+  }
+  throw new AuthenticationError('You are not authenticated');
+}
+
+module.exports = {categories, categoryTypes};
--- server/schemas/resolvers/query/index.js
+++ server/schemas/resolvers/query/index.js
@@ -1,14 +1,18 @@
 const user = require('./user');
 const payees = require('./payee');
-const transactions = require('./transaction');
+const { categories, categoryTypes } = require('./category');
+const {transaction, transactions} = require('./transaction');
 const { account, accounts } = require('./account');
 
 const Query = {
   user,
   payees,
+  categories,
+  categoryTypes,
   account,
   accounts,
+  transaction,
   transactions
 };
-console.log('Query', Query);
+
 module.exports = Query;
