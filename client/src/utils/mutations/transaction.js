454f53c518a947e527ef46cfec5973242324d2b3
Brian Whisler <brian.whisler@gmail.com>
2024-02-29 19:02:09 -0700

    simplify updateTransaction mutation

--- client/src/utils/mutations/transaction.js
+++ client/src/utils/mutations/transaction.js
@@ -43,45 +43,35 @@ export const ADD_TRANSACTION = gql`
 `;
 
 export const UPDATE_TRANSACTION = gql`
-  mutation updateTransaction(
-    $transactionId: ID!
-    $account: ID
-    $purchaseDate: String
-    $payee: ID
-    $category: ID
-    $amount: Float
-    $split: Boolean
-    $related: ID
-    $cleared: Boolean
-  ) {
-    updateTransaction(
-      transactionId: $transactionId
-      account: $account
-      purchaseDate: $purchaseDate
-      payee: $payee
-      category: $category
-      amount: $amount
-      split: $split
-      related: $related
-      cleared: $cleared
-    ) {
-      _id
+  mutation UpdateTransaction($updateTransactionInput: updateTransactionInput) {
+    updateTransaction(updateTransactionInput: $updateTransactionInput) {
+      code
+      success
+      message
+      transactions {
+        _id
+        purchaseDate
+        payee {
+          _id
+          payeeName
+        }
+        category {
+          _id
+          categoryName
+          categoryType {
+            _id
+            categoryTypeName
+          }
+        }
+        amount
+        cleared
+        account {
+          _id
+        }
+      }
       account {
         _id
       }
-      purchaseDate
-      payee {
-        _id
-      }
-      category {
-        _id
-      }
-      amount
-      split
-      related {
-        _id
-      }
-      cleared
     }
   }
 `;
@@ -92,15 +82,18 @@ export const REMOVE_TRANSACTION = gql`
       code
       message
       success
-      transaction {
+      transactions {
         _id
         purchaseDate
         payee {
+          _id
           payeeName
         }
         category {
+          _id
           categoryName
           categoryType {
+            _id
             categoryTypeName
           }
         }
