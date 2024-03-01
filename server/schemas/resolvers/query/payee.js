aabfe293000329c6c6f90ea50c9e653202f96c43
Brian Whisler <brian.whisler@gmail.com>
2024-02-29 18:54:09 -0700

    sort query

--- server/schemas/resolvers/query/payee.js
+++ server/schemas/resolvers/query/payee.js
@@ -1,10 +1,9 @@
 const { Payee } = require("../../../models");
 const { AuthenticationError } = require("../../../utils/auth");
 
-const payees = async (parent, args, context) => {
-  console.log(context.user._id);
+const payees = async (_, __, context) => {
   if (context.user) {
-    let userPayees = await Payee.find({ user: context.user._id });
+    let userPayees = await Payee.find({ user: context.user._id }).sort({ payeeName: 1 });
     return userPayees;
   }
   throw new AuthenticationError('You are not authenticated');
