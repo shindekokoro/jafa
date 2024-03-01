901e5c9b7f37bc2919a1fa11e1dfe8f426ff3471
Brian Whisler <brian.whisler@gmail.com>
2024-02-29 18:53:40 -0700

    find one transaction

--- server/schemas/resolvers/query/transaction.js
+++ server/schemas/resolvers/query/transaction.js
@@ -1,6 +1,13 @@
 const { Transaction } = require('../../../models');
 
-const transactions = async (parent, { accountId }) => {
+const transaction = async (_, { accountId, transactionId }) => {
+  let transaction = await Transaction.findOne({ _id: transactionId, account: accountId })
+    .populate({ path: 'payee' })
+    .populate({ path: 'category', populate: { path: 'categoryType' } });
+  return transaction;
+}
+
+const transactions = async (_, { accountId }) => {
   let transactions = await Transaction.find({ account: accountId })
     .populate({ path: 'payee' })
     .populate({ path: 'category', populate: { path: 'categoryType' } })
@@ -8,4 +15,4 @@ const transactions = async (parent, { accountId }) => {
   return transactions;
 }
 
-module.exports = transactions;
+module.exports = {transaction, transactions};
