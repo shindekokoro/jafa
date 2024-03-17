5caa4c4a728a2f212426069fecb9109f774f45c2
Brian Whisler <brian.whisler@gmail.com>
2024-03-17 07:08:18 -0600

    Account component

---
+++ client/src/components/Account.jsx
@@ -0,0 +1,132 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import {
+  Backdrop,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  Fade,
+  IconButton,
+  Modal,
+  Typography
+} from '@mui/material';
+import DeleteForever from '@mui/icons-material/DeleteForever';
+
+import { REMOVE_ACCOUNT } from '../utils/mutations';
+
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4
+};
+
+export default function Account({ account, setUserAccounts }) {
+  const [open, setOpen] = useState(false);
+  const openModel = () => setOpen(true);
+  const closeModel = () => setOpen(false);
+
+  const [removeAccount] = useMutation(REMOVE_ACCOUNT);
+
+  const handleDeleteAccount = async (event, account) => {
+    // event.preventDefault(event);
+    event.stopPropagation();
+    
+    console.log('Attempting to delete account', account.accountName);
+    let removedAccount;
+    try {
+      let { data } = await removeAccount({
+        variables: {
+          account: account._id
+        }
+      });
+      removedAccount = data.removeAccount;
+    } catch (error) {
+      console.error(error);
+    }
+    if (removedAccount?.success) {
+      console.log('Account removed');
+      removedAccount.accounts
+        ? setUserAccounts(removedAccount.accounts)
+        : console.log('No accounts');
+      return closeModel();
+    } else {
+      return console.error('Account not removed', removedAccount);
+    }
+    
+  };
+
+  const DeleteModal = () => {
+    return (
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        open={open}
+        onClose={closeModel}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            TransitionComponent: Fade
+          }
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={modalStyle}>
+            <Typography id="spring-modal-title" variant="h6" component="h2">
+              Are you sure you want to delete this account?
+            </Typography>
+            <Typography id="spring-modal-description" variant="body1">
+              {account.accountName} at {account.institution.institutionName}
+              <br />
+              {account._id}
+            </Typography>
+            <Button onClick={(event) => handleDeleteAccount(event, account)}>Yes</Button>
+            <Button onClick={closeModel}>No</Button>
+          </Box>
+        </Fade>
+      </Modal>
+    );
+  };
+
+  return (
+    <>
+      <Card variant="outlined">
+        <CardHeader
+          title={account.accountName}
+          subheader={account.institution.institutionName}
+          action={
+            <IconButton aria-label="delete-account" size="small" onClick={openModel}>
+              <DeleteForever fontSize="inherit" />
+            </IconButton>
+          }
+        />
+        <DeleteModal />
+        <CardContent>
+          <Typography variant="body2">{account.description || <br /> }</Typography>
+        </CardContent>
+        <Divider />
+        <CardActions>
+          <Button
+            size="small"
+            variant="outlined"
+            component={Link}
+            to={`/account/${account._id}`}
+          >
+            More Info
+          </Button>
+        </CardActions>
+      </Card>
+    </>
+  );
+}
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,7 @@
 import Header from './Header';
 import Signup from './Signup';
+import Account from './Account';
 import TransactionsTable from './TransactionsTable';
+import SelectedTransactionRow from './SelectedTransactionRow';
 
-export { Header, Signup, TransactionsTable };
+export { Account, Header, Signup, SelectedTransactionRow, TransactionsTable };
