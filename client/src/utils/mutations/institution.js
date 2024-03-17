64bcbc4513fbb24fa26a58518f94e712cbc54671
Brian Whisler <brian.whisler@gmail.com>
2024-03-17 07:20:49 -0600

    return more info for institution gql

--- client/src/utils/mutations/institution.js
+++ client/src/utils/mutations/institution.js
@@ -1,26 +1,26 @@
 import { gql } from '@apollo/client';
 
 export const ADD_INSTITUTION = gql`
-  mutation addInstitution($name: String!, $otherInfo: String) {
-    addInstitution(name: $name, otherInfo: $otherInfo) {
-      _id
-      name
-      otherInfo
+  mutation AddInstitution($institutionInput: institutionInput!) {
+    addInstitution(institutionInput: $institutionInput) {
+      code
+      success
+      message
+      institution {
+        _id
+        institutionName
+      }
+      institutions {
+        _id
+        institutionName
+      }
     }
   }
 `;
 
 export const UPDATE_INSTITUTION = gql`
-  mutation updateInstitution(
-    $institutionId: ID!
-    $name: String!
-    $otherInfo: String
-  ) {
-    updateInstitution(
-      institutionId: $institutionId
-      name: $name
-      otherInfo: $otherInfo
-    ) {
+  mutation updateInstitution($institutionId: ID!, $name: String!, $otherInfo: String) {
+    updateInstitution(institutionId: $institutionId, name: $name, otherInfo: $otherInfo) {
       _id
       name
       otherInfo
