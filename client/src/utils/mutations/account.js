1d67c63905231e65ee36dcf86487bbac51fba156
Brian Whisler <brian.whisler@gmail.com>
2024-03-17 07:17:44 -0600

    addAccount, updateAccount gql mutations

--- client/src/utils/mutations/account.js
+++ client/src/utils/mutations/account.js
@@ -1,60 +1,119 @@
 import { gql } from '@apollo/client';
 
 export const ADD_ACCOUNT = gql`
-  mutation addAccount(
-    $accountName: String!
-    $description: String
-    $institution: ID!
-    $type: String!
-    $currency: String!
-    $startingBalance: Float!
-  ) {
-    addAccount(
-      accountName: $accountName
-      description: $description
-      institution: $institution
-      type: $type
-      currency: $currency
-      startingBalance: $startingBalance
-    ) {
-      _id
-      accountName
-      description
-      institution {
+  mutation AddAccount($addAccountInput: addAccountInput) {
+    addAccount(addAccountInput: $addAccountInput) {
+      code
+      success
+      message
+      account {
         _id
+        accountName
+        currency
+        description
+        institution {
+          _id
+          institutionName
+          otherInfo
+        }
+        transactions {
+          _id
+          purchaseDate
+          cleared
+          payee {
+            _id
+            payeeName
+          }
+          category {
+            categoryName
+          }
+          categoryType {
+            categoryTypeName
+          }
+          amount
+        }
       }
-      type
-      currency
-      startingBalance
+      accounts {
+        _id
+        accountName
+        currency
+        description
+        institution {
+          _id
+          institutionName
+          otherInfo
+        }
+        transactions {
+          _id
+          purchaseDate
+          cleared
+          payee {
+            _id
+            payeeName
+          }
+          category {
+            categoryName
+          }
+          categoryType {
+            categoryTypeName
+          }
+          amount
+        }
+      }
     }
   }
 `;
 
 export const REMOVE_ACCOUNT = gql`
-  mutation removeAccount($account: ID!) {
+  mutation RemoveAccount($account: ID!) {
     removeAccount(account: $account) {
-      _id
+      code
+      success
+      message
+      account {
+        _id
+        accountName
+        description
+        institution {
+          _id
+        }
+        type
+        currency
+        startingBalance
+      }
+      accounts {
+        _id
+        accountName
+        description
+        institution {
+          _id
+          institutionName
+        }
+        type
+        currency
+        startingBalance
+      }
     }
   }
 `;
 
 export const UPDATE_ACCOUNT = gql`
   mutation updateAccount(
-    $account: ID!,
-    $accountName: String,
-    $description: String,
-    $institution: ID,
-    $type: String,
-    $currency: String,
+    $account: ID!
+    $accountName: String
+    $description: String
+    $institution: ID
+    $type: String
+    $currency: String
     $startingBalance: Float
   ) {
     updateAccount(
-      account: $account,
-      accountName: $accountName,
-      description: $description,
-      institution: $institution,
-      type: $type,
-      currency: $currency,
+      account: $account
+      accountName: $accountName
+      description: $description
+      institution: $institution
+      type: $type
+      currency: $currency
       startingBalance: $startingBalance
     ) {
       _id
